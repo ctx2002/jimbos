2bcc93b73b623bf25a55ff10704795a714fcb143
anru chen <ctx2002@gmail.com>
2015-10-16 10:45:46 +1300

    change to client_id from mail_chimp_id

--- database/migrations/2015_10_08_082032_create_jimbo_tables.php
+++ database/migrations/2015_10_08_082032_create_jimbo_tables.php
@@ -15,9 +15,9 @@ class CreateJimboTables extends Migration
         Schema::create('coupons', function (Blueprint $table) {
             $table->engine = 'InnoDB';
             $table->increments('id');
-            $table->string('mail_chimp_data_id', 10);
+            $table->string('client_id', 10);
             $table->string('mail_chimp_list_id', 10);
-            $table->string('tcc_id', 10);
+            $table->string('tcc_id', 255);
             $table->date('pet_dob')->nullable();
             $table->timestamp('added_on')->default(DB::raw('CURRENT_TIMESTAMP'));
         });
