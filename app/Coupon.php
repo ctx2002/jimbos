ff4b6ed4f034720cb34a13d3c5f69a1ce25e4d5f
anru chen <ctx2002@gmail.com>
2015-10-20 13:23:26 +1300

    init

--- app/Coupon.php
+++ app/Coupon.php
@@ -1,7 +1,7 @@
 <?php
-
 namespace App;
 
+use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\Model;
 
 class Coupon extends Model
@@ -9,4 +9,18 @@ class Coupon extends Model
     protected $table = 'coupons';
     
     public $timestamps = false;
+    
+    public static function loadCoupon(Request $request)
+    {
+        $coupon = new \App\Coupon();
+        $data = $request->input('data');
+        
+        $coupon->client_id = $data['id'];
+        $coupon->campaign_id = $data['merges']['campaign_uid'];
+        $coupon->fname = $data['merges']['FNAME'];
+        $coupon->lname = $data['merges']['LNAME'];
+        $coupon->email = $data['merges']['EMAIL'];
+        $coupon->pet_dob = isset($data['merges']['pet_dob'])? $data['merges']['pet_dob'] : null; 
+        return $coupon;
+    }
 }
