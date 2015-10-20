b2490605d5af547b007728305d4fb09a856d03bd
anru chen <ctx2002@gmail.com>
2015-10-20 13:24:33 +1300

    rewrote db schema

--- database/migrations/2015_10_08_082032_create_jimbo_tables.php
+++ database/migrations/2015_10_08_082032_create_jimbo_tables.php
@@ -17,15 +17,15 @@ class CreateJimboTables extends Migration
             $table->increments('id');
             $table->string('client_id', 10);
             $table->string('campaign_id', 32);
-            $table->string('mail_chimp_list_id', 10);
+            //$table->string('mail_chimp_list_id', 10);
             $table->string('tcc_id', 255);
             $table->date('pet_dob')->nullable();
             $table->string('fname',64);
             $table->string('lname',64);
             $table->string('email',64);
             $table->timestamp('added_on')->default(DB::raw('CURRENT_TIMESTAMP'));
-            $table->date('downloding_link_date')->nullable();
-            $table->string('type',16);
+            //$table->date('downloding_link_date')->nullable();
+            //$table->string('type',16);
         });
     }
 
